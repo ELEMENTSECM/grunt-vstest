ad7676db529a19624c56167bb7e7c9be5e7d2d39
Marius Grande Andersen <mga@gecko.no>
2014-12-19 16:33:36 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,62 @@
+# grunt-vstest
+
+> Run tests using the Visual Studio test runner via Grunt
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`. In other words it should work on 0.4.5 or higher.
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-vstest --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-vstest');
+```
+
+## The "vstest" task
+
+### Overview
+In your project's Gruntfile, add a section named `vstest` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+    vstest: {
+        dev: {
+            src: ["Example.Tests/bin/Release/*Tests.dll"], // Relative path to test dll(s)
+			options: {
+				// Listing all currently supported options
+				settings: "Local.RunSettings",
+				inIsolation: true,
+				platform: "x64",
+				framework: "framework45", // Valid values are Framework35, Framework40 and Framework45
+				testCaseFilter: "TestCategory!=KnownError"
+			}
+        }
+    }
+});
+```
+
+## Contributing
+We appreciate the community submiting issues and pull requests. 
+
+## Issues and installing previous versions
+
+If you have any problems with the latest release please log an issue at https://github.com/GeckoInformasjonssystemerAS/grunt-vstest/issues.
+
+If you need to roll back to an earlier version you can use the following syntax to install a specific version
+
+```
+npm install grunt-vstest@0.1.2
+```
+
+Also see https://www.npmjs.org/doc/json.html#dependencies for details of how to specify a particular package version in your package.json file
+
+## Release Notes
+
+|Version| Notes|
+|-------|------|
+|0.0.1|Initial commit allowing you to run tests via grunt using the Visual Studio test runner.
---
+++ tasks/vstest.js
@@ -0,0 +1,116 @@
+/*
+* grunt-vstest
+*
+* Inspired by https://github.com/mrjackdavis/grunt-mstest
+*
+* Copyright (c) 2014 Steinar Herland, Gecko Informasjonsystemer AS
+* Licensed under the MIT license.
+*/
+
+'use strict';
+var spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+// Please see the Grunt documentation for more information regarding task
+// creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('vstest', 'Run tests using the Visual Studio test runner via Grunt', function() {
+
+        var done = this.async();
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            vstestPath: getExePath(),
+            force:false
+        });
+
+        function gruntWarn(str){
+            if(options.force)
+                grunt.log.write(str);
+            else
+                grunt.fail.warn(str);
+        }
+
+        var args = [];
+
+        this.filesSrc.map(function(filePath){
+            args.push(filePath);
+        });
+
+    	//See http://msdn.microsoft.com/en-us/library/jj155796.aspx for command line description
+
+        if (options.settings) {
+        	args.push("/Settings:" + options.settings);
+        }
+
+        if (options.inIsolation) {
+			args.push("/InIsolation");
+		}
+
+		if (options.platform) {
+			args.push("/Platform:" + options.platform);
+		}
+
+		if (options.framework) {
+			args.push("/Framework:" + options.framework); //Valid values are Framework35, Framework40 and Framework45.
+		}
+
+		if (options.testCaseFilter) {
+			args.push("/TestCaseFilter:" + options.testCaseFilter);
+		}
+
+        var process = spawn(options.vstestPath,args);
+
+	    process.stdout.on('data', function (data) {
+		    grunt.log.write(/*'stdout...' +*/ data)
+        });
+	    process.stderr.on('data', function (data) {
+	        grunt.log.error(/*'stderr...' +*/ data);
+        });
+
+	    process.on('exit', function (code) {
+            if (code !== 0) {
+                gruntWarn('Some tests have failed');
+            }
+            done();
+        });
+    });
+
+
+    function getExePath() {
+        //Possible env variables for visual studio tools, in reverse order of priority
+        var vsToolsArr = [process.env.VS100COMNTOOLS,process.env.VS110COMNTOOLS,process.env.VS120COMNTOOLS]
+
+        //Get highest priority VS tools
+        var vsTools = null;
+        for (var i = vsToolsArr.length - 1; i >= 0; i--) {
+            var item = vsToolsArr[i]
+            if(item && item != ""){
+                vsTools = item;
+                break;
+            }
+        };
+
+        if(!vsTools)
+            grunt.fatal("Visual studio tools not installed")
+
+    	//var exePath = path.join(vsTools, "../IDE", 'MSTest.exe');
+    	var exePath = path.join(vsTools, "../IDE/CommonExtensions/Microsoft/TestWindow", 'vstest.console.exe');
+        
+
+        if (!fs.existsSync(exePath)) {
+        	grunt.fatal('Unable to find vstest.console.exe executable at ' + exePath + " use the option 'vstestPath' to override");
+        }
+
+        return exePath;
+    }
+
+    function escapeShell(cmd) {
+      return '"'+cmd+'"';
+    };
+};
+
+
